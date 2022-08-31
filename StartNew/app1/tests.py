9ff80c225e8b21f72b4b3f3ac91b7c9968613df8
MuhammadyunusTursunov <muhammadyunus.tursunov@gmail.com>
2022-08-31 10:54:26 +0500

    test ichiga birnima qo'shdim

--- StartNew/app1/tests.py
+++ StartNew/app1/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+def hello():
+    print("Helloooo")
+
+
+def hello1():
+    print("Helloooo")
+    print("Helloooo")
+    print("Helloooo")
+
+
+def hello2():
+    print("Helloooo")
+    print("Helloooo")
+    print("Helloooo")
